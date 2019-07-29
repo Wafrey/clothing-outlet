46d361dfdcf51875621881c8aae1e5f2996275a7
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-07-29 17:06:34 +0300

    finish styling menu-item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,8 +47,8 @@ class Directory extends Component{
         return(
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id}) =>(
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} />
+                    this.state.sections.map(({title, imageUrl, id, size}) =>(
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                     ))
                 }
             </div>
