6ef3b954ea406b3b4ea21492b63dedd12e3c013e
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-07-30 13:27:18 +0300

    Add collection-item/preview component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionItem from './../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({title, items}) => (
@@ -6,8 +7,8 @@ const CollectionPreview = ({title, items}) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.filter((item, idx) => idx < 4).map((item)=>(
-                    <div key={item.id}>{item.name}</div>
+                items.filter((item, idx) => idx < 4).map(({id, name, imageUrl, price})=>(
+                    <CollectionItem key={id} name={name} imageUrl={imageUrl} price={price}></CollectionItem>
                 ))
             }
         </div>
