faef1e830fa22b9115a75b6ff72b213217d54a12
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-05-11 20:42:27 +0300

    update shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,14 @@
-import React, {Component} from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from './../../components/collection-preview/collection-preview.component';
+import React from "react";
+import { Route } from "react-router-dom";
 
-class ShopPage extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            collections: SHOP_DATA
-        }
-    }
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-    render(){
-        const {collections} = this.state;
-        return(
-            <div className='shop-page'>
-                {
-                    collections.map(({id, title, routeName, items}) => (
-                        <CollectionPreview key={id} title={title} routeName={routeName} items={items}/>
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,6 +3,8 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducers";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 const persistConfig = {
   key: "root",
@@ -13,6 +15,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
