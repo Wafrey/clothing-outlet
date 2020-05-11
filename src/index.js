3a409a299f723e200153911b062fa17fe1b2650c
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-05-11 15:13:10 +0300

    add redux-persist to handle cart product when refresh

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(...middlewares),
@@ -12,4 +13,6 @@ const store = createStore(
   )
 );
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
