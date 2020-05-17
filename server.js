9f65bb08ba1a264a3403f45238099ce5c075533f
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2020-05-17 17:42:05 +0300

    change some bugs

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path, join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
